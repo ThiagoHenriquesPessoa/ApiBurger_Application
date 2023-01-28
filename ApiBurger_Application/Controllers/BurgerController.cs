2dc034f1bc55fa9ab3f74ddb511702ab52b584b1
Thiago Pessoa <thiagokxl5@gmail.cpm>
2023-01-27 23:56:43 -0300

    Criando a estrutura inicial da api

---
+++ ApiBurger_Application/Controllers/BurgerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiBurger_Application.Controllers
+{
+    public class BurgerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ApiBurger_Application/Controllers/StatusController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiBurger_Application.Controllers
+{
+    public class StatusController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
