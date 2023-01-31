8811ace3eef536de1cc21dbd2349997690804c52
Thiago Pessoa <thiagokxl5@gmail.cpm>
2023-01-30 22:49:02 -0300

    Criando o business do meat

---
+++ ApiBurger_Business/InterfaceBusiness/IMeatBusiness.cs
@@ -0,0 +1,13 @@
+﻿using ApiBurger_Domain.Entities;
+
+namespace ApiBurger_Business.InterfaceBusiness
+{
+    public interface IMeatBusiness
+    {
+        List<Meat> GetAllMeat();
+        Meat GetMeat(int id);
+        Task<int> SaveMeat(Meat meat);
+        Task<int> UpdateMeat(Meat meat);
+        Task<int> DeleteMeat(int id);
+    }
+}
