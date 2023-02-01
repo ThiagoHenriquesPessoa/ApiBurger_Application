5b228be63838a7d1dadf9b62fff729d008adcbb9
Thiago Pessoa <thiagokxl5@gmail.cpm>
2023-01-31 21:47:43 -0300

    Create Repository of Optional

---
+++ ApiBurger_Repository/InterfaceRepositories/IOptionalRepository.cs
@@ -0,0 +1,13 @@
+﻿using ApiBurger_Domain.Entities;
+
+namespace ApiBurger_Repository.InterfaceRepositories
+{
+    public interface IOptionalRepository
+    {
+        List<Optional> GetAllOptional();
+        Optional GetOptional(int id);
+        Task<int> SaveOptional(Optional optional);
+        Task<int> UpdateOptional(Optional optional);
+        Task<int> DeleteOptional(int id);
+    }
+}
