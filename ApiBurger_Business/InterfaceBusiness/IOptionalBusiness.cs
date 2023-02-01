773d7c4d07a9ae42389b3e7c4178ab2691652733
Thiago Pessoa <thiagokxl5@gmail.cpm>
2023-01-31 22:00:47 -0300

    Create Business of Optional

---
+++ ApiBurger_Business/InterfaceBusiness/IOptionalBusiness.cs
@@ -0,0 +1,13 @@
+﻿using ApiBurger_Domain.Entities;
+
+namespace ApiBurger_Business.InterfaceBusiness
+{
+    public interface IOptionalBusiness
+    {
+        List<Optional> GetAllOptional();
+        Optional GetOptional(int id);
+        Task<int> SaveOptional(Optional optional);
+        Task<int> UpdateOptional(Optional optional);
+        Task<int> DeleteOptional(int id);
+    }
+}
