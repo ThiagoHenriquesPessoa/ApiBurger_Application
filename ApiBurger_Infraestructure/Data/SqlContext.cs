bc8d62b7cc3297f12c2e6d06b8ffc6b6713b11ab
Thiago Pessoa <thiagokxl5@gmail.cpm>
2023-02-03 23:11:58 -0300

    criacao de classe Optionais, OptionaisRepository, IOptionaisRepository, BurgerRepository e IBurgerRepository

--- ApiBurger_Infraestructure/Data/SqlContext.cs
+++ ApiBurger_Infraestructure/Data/SqlContext.cs
@@ -13,7 +13,7 @@ namespace ApiBurger_Infraestructure.Data
         public DbSet<Bread> Bread { get; set; }
         public DbSet<Meat> Meat { get; set; }
         public DbSet<Optional> Optional { get; set; }
-        public DbSet<Status> Status { get; set; }
         public DbSet<Burger> Burger { get; set; }
+        public DbSet<Optionais> Optionais { get; set; }
     }
 }
---
+++ ApiBurger_Repository/Repositories/OptionaisRepository.cs
@@ -0,0 +1,29 @@
+﻿using ApiBurger_Domain.Entities;
+using ApiBurger_Infraestructure.Data;
+using ApiBurger_Repository.InterfaceRepositories;
+
+namespace ApiBurger_Repository.Repositories
+{
+    public class OptionaisRepository : IOptionaisRepository
+    {
+        private readonly SqlContext _sqlContext;
+
+        public OptionaisRepository(SqlContext sqlContext)
+        {
+            _sqlContext = sqlContext;
+        }
+        public async Task<int> SaveOptionais(Optionais optionais)
+        {
+            try
+            {
+                await _sqlContext.Optionais.AddAsync(optionais);
+                await _sqlContext.SaveChangesAsync();
+                return 1;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+    }
+}
